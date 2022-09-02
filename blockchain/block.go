cdd0a47ed3e3d8eee60345f075470673faf141a8
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-02 20:23:37 +0430

    change package

---
+++ blockchain/block.go
@@ -0,0 +1,24 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DriveHash()
+	return block
+}
