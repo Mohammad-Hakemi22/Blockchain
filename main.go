21896216bb155d45bf24b3609e8ec61a127a2db3
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-02 23:58:03 +0430

    run bc

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/mohammad-hakemi22/blockchain/blockchain"
+)
 
 func main() {
-	fmt.Println("Hello BC")
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First Block!")
+	chain.AddBlock("Second Block!")
+	chain.AddBlock("Third Block!")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Pervious Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 }
