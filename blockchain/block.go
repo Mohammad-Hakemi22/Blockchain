188405b42e87dfed429f20667345fc9ee705c35d
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-02 23:57:40 +0430

    fix add block

--- blockchain/block.go
+++ blockchain/block.go
@@ -25,7 +25,7 @@ func CreateBlock(data string, prevHash []byte) *Block {
 
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.PrevHash)
+	newBlock := CreateBlock(data, prevBlock.Hash)
 	chain.Blocks = append(chain.Blocks, newBlock)
 }
 
