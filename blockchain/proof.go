75d4784ec44d1243812b6bb901f86baf622a8b97
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-05 22:01:47 +0430

    create pow and get data from block

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,3 +1,29 @@
 package blockchain
 
-// get date from block
+import (
+	"bytes"
+	"math/big"
+
+	"github.com/mohammad-hakemi22/blockchain/utility"
+)
+
+const Difficulty = 12
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+	return &ProofOfWork{b, target}
+}
+
+func (pow *ProofOfWork) InitDate(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash, pow.Block.Data, utility.ToHex(int64(nonce)), utility.ToHex(int64(Difficulty))},
+		[]byte{})
+	return data
+}
