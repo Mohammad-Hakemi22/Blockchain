2d76736601ace36c1ba603afb4868aca51da408e
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-06 21:17:14 +0430

    add validation func

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -47,3 +47,11 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	}
 	return nonce, hash[:]
 }
+
+func (pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+	data := pow.InitDate(pow.Block.Nonce)
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+	return intHash.Cmp(pow.Target) == -1
+}
