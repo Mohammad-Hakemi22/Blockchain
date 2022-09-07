51333baaf377357c8dd009c7cd24070053b9d162
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-07 22:28:53 +0430

    transfer blockchain functions to blockchain.go

--- blockchain/block.go
+++ blockchain/block.go
@@ -23,18 +23,8 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, newBlock)
-}
-
 func GenesisBlock() *Block {
 	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{GenesisBlock()}}
 }
 
 func (b *Block) Serialize() []byte {
