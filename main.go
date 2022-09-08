81f2a7e12deaa2b6ccb6ee759995d16ff4d8f5db
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-08 23:10:30 +0430

    run cli in main

--- main.go
+++ main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+	"os"
 
 	"github.com/mohammad-hakemi22/blockchain/blockchain"
+	cmd "github.com/mohammad-hakemi22/blockchain/commandline"
 )
 
 func main() {
+	defer os.Exit(0)
 	chain := blockchain.InitBlockChain()
+	defer chain.Database.Close()
 
-	chain.AddBlock("First Block!")
-	chain.AddBlock("Second Block!")
-	chain.AddBlock("Third Block!")
-
-	for _, block := range chain.Blocks {
-		fmt.Printf("Pervious Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := blockchain.NewProof(block)
-		fmt.Printf("POW: %s\n", strconv.FormatBool(pow.Validate()))
-	}
+	cli := cmd.CommandLine{Blockchain: chain}
+	cli.Run()
 }
