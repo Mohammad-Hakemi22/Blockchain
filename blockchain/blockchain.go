25c1a7551ed2c3454708da8e4b4a43da78fc93e3
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-09 22:12:29 +0430

    split initblockchain to two functions

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -2,13 +2,17 @@ package blockchain
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 
 	badger "github.com/dgraph-io/badger/v3"
 	"github.com/mohammad-hakemi22/blockchain/utility"
 )
 
 const (
-	dbPath = "./tmp/blocks"
+	dbPath      = "./tmp/blocks"
+	dbFile      = "./tmp/blocks/MANIFEST"
+	genesisData = "First Transaction from genesis block"
 )
 
 type BlockChain struct {
@@ -21,33 +25,60 @@ type BlockchainIterator struct {
 	Database    *badger.DB
 }
 
-func InitBlockChain() *BlockChain {
+func DBexists() bool {
+	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func InitBlockChain(address string) *BlockChain {
+	if DBexists() {
+		fmt.Println("Blockchain already exists.")
+		runtime.Goexit()
+	}
 	var lastHash []byte
 	opt := badger.DefaultOptions(dbPath)
 	opt.ValueDir = dbPath
 	opt.Dir = dbPath
-
 	db, err := badger.Open(opt)
 	utility.ErrorHandler("something wrong in opening database", err)
+
 	err = db.Update(func(txn *badger.Txn) error {
-		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound { // check for existing blockchain
-			fmt.Println("Not existing Blockchain!")
-			genesis := GenesisBlock()
-			fmt.Println("Genesis created!")
-			err := txn.Set(genesis.Hash, genesis.Serialize())
-			utility.ErrorHandler("can't set block in database", err)
-			err = txn.Set([]byte("lh"), genesis.Hash)
-			lastHash = genesis.Hash
-			return err
-		} else {
-			item, err := txn.Get([]byte("lh"))
-			utility.ErrorHandler("can't get last hash", err)
-			err = item.Value(func(val []byte) error {
-				lastHash = val
-				return nil
-			})
-			return err
-		}
+		cbtx := CoinbaseTx(address, genesisData)
+		genesis := GenesisBlock(cbtx)
+		fmt.Println("Genesis created!")
+		err := txn.Set(genesis.Hash, genesis.Serialize())
+		utility.ErrorHandler("can't set block in database", err)
+		err = txn.Set([]byte("lh"), genesis.Hash)
+		lastHash = genesis.Hash
+		return err
+	})
+	utility.ErrorHandler("can't do R/W operation on database", err)
+	blockchain := BlockChain{LastHash: lastHash, Database: db}
+	return &blockchain
+}
+
+func ContinueBlockchain(address string) *BlockChain {
+	if !DBexists() {
+		fmt.Println("No existing Blockchain, created one")
+		runtime.Goexit()
+	}
+	var lastHash []byte
+	opt := badger.DefaultOptions(dbPath)
+	opt.ValueDir = dbPath
+	opt.Dir = dbPath
+	db, err := badger.Open(opt)
+	utility.ErrorHandler("something wrong in opening database", err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		utility.ErrorHandler("can't get block from database", err)
+		item.Value(func(val []byte) error {
+			lastHash = val
+			return nil
+		})
+		return err
 	})
 	utility.ErrorHandler("can't do R/W operation on database", err)
 	blockchain := BlockChain{LastHash: lastHash, Database: db}
