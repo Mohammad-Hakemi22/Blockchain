d1f7641626cdb191651cfa0e83c17f71a57a6ee5
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-10 22:10:06 +0430

    update functions-> data with transaction

--- blockchain/block.go
+++ blockchain/block.go
@@ -2,20 +2,31 @@ package blockchain
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
 
 	"github.com/mohammad-hakemi22/blockchain/utility"
 )
 
 type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-	Nonce    int
+	Hash         []byte
+	Transactions []*Transaction
+	PrevHash     []byte
+	Nonce        int
 }
 
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+	return txHash[:]
+}
+
+func CreateBlock(txs []*Transaction, prevHash []byte) *Block {
+	block := &Block{[]byte{}, txs, prevHash, 0}
 	pow := NewProof(block)
 	nonce, hash := pow.Run()
 	block.Nonce = nonce
@@ -23,8 +34,8 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func GenesisBlock() *Block {
-	return CreateBlock("Genesis", []byte{})
+func GenesisBlock(coinbase *Transaction) *Block {
+	return CreateBlock([]*Transaction{coinbase}, []byte{})
 }
 
 func (b *Block) Serialize() []byte {
