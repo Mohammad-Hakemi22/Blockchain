ed556a7b7d9e3cc6ced611690288e01fcb94e850
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-11 22:19:07 +0430

    update main

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"os"
 
-	"github.com/mohammad-hakemi22/blockchain/blockchain"
 	cmd "github.com/mohammad-hakemi22/blockchain/commandline"
 )
 
 func main() {
 	defer os.Exit(0)
-	chain := blockchain.InitBlockChain()
-	defer chain.Database.Close()
-
-	cli := cmd.CommandLine{Blockchain: chain}
+	cli := cmd.CommandLine{}
 	cli.Run()
 }
