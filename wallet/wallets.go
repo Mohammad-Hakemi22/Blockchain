9da48cba286763c6d1ebd2da03d8e5c2345c5ff4
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-09-15 22:45:21 +0430

    load and save function

--- wallet/wallets.go
+++ wallet/wallets.go
@@ -5,6 +5,7 @@ import (
 	"crypto/elliptic"
 	"encoding/gob"
 	"io/ioutil"
+	"os"
 
 	"github.com/mohammad-hakemi22/blockchain/utility"
 )
@@ -23,4 +24,19 @@ func (ws *Wallets) SaveFile() {
 	utility.ErrorHandler("can't encode wallets", err)
 	err = ioutil.WriteFile(walletFile, content.Bytes(), 0644)
 	utility.ErrorHandler("can't write wallets file", err)
+}
+
+func (ws *Wallets) LoadFile() error {
+	if _, err := os.Stat(walletFile); os.IsNotExist(err) {
+		return err
+	}
+	var wallets Wallets
+	fileContent, err := ioutil.ReadFile(walletFile)
+	utility.ErrorHandler("can't load wallets file", err)
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+	gob.Register(elliptic.P256())
+	err = decoder.Decode(&wallets)
+	utility.ErrorHandler("can't decode wallets content", err)
+	ws.Wallets = wallets.Wallets
+	return nil
 }
